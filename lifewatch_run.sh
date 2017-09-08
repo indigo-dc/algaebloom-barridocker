3a363b17dd64f2677b6e12be4d07077fa69ad366
ferag <ferag@live.com>
2017-09-08 09:17:00 +0200

    Update lifewatch_run.sh

--- lifewatch_run.sh
+++ lifewatch_run.sh
@@ -39,8 +39,52 @@ fi
 
 echo Run test
 # Run Rscript
-./run_delwaq.sh > log.txt  || exit 1
+# ./run_delwaq.sh > log.txt  || exit 1
+inpfile=$INPUT_CONFIG_FILE
 
+currentdir=`pwd`
+echo $currentdir
+argfile=$currentdir/$inpfile
+
+    #
+    # Set the directory containing delwaq1 and delwaq2 and
+    # the directory containing the proc_def and bloom files here
+    #
+exedir=$D3D_BIN/bin/lnx64/waq/bin
+export LD_LIBRARY_PATH=$exedir:$LD_LIBRARY_PATH
+procfile=$D3D_BIN/bin/lnx64/waq/default/proc_def
+
+    #
+    # Run delwaq 1
+    #
+$exedir/delwaq1 $argfile -eco $D3D_BIN/bin/lnx64/waq/default/bloom.spe -p "$procfile"
+
+    #
+    # Wait for any key to run delwaq 2
+    #
+if [ $? == 0 ]
+  then
+    echo ""
+    echo "Delwaq1 did run without errors."
+
+    #
+    # Run delwaq 2
+    #
+    echo ""
+    $exedir/delwaq2 $argfile
+
+    if [ $? -eq 0 ]
+      then
+        echo ""
+        echo "Delwaq2 did run without errors."
+      else
+        echo ""
+        echo "Delwaq2 did not run correctly."
+    fi
+else
+    echo ""
+    echo "Delwaq1 did not run correctly, ending calculation"
+fi
 sleep 3
 
 # Collect output
